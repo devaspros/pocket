83e7c1dde4055fbb37fbfca987b6601f08493098
Francisco Quintero Coronell <frajaquico@yahoo.es>
2021-06-03 11:11:23 -0500

    Install and configure dotenv gem

--- lib/pocket.rb
+++ lib/pocket.rb
@@ -1,6 +1,12 @@
+require 'dotenv/load'
 require "pocket/version"
 
 module Pocket
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Client
+  	def hola
+      puts ENV['POCKET_CONSUMER_KEY']
+  	end
+  end
 end
