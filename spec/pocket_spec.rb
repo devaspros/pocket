20120a793d01a94fcc22325ff9ebe3a1a7f68ad9
Francisco Quintero Coronell <frajaquico@yahoo.es>
2021-08-02 21:12:36 -0500

    First test setup

--- spec/pocket_spec.rb
+++ spec/pocket_spec.rb
@@ -1,5 +1,16 @@
-RSpec.describe Pocket do
-  it "has a version number" do
-    expect(Pocket::VERSION).not_to be nil
+# spec/pocket_spec.rb
+
+RSpec.describe Pocket::Client do
+  let(:client) { described_class.new(consumer_key: 'hola', access_token: 'mundo') }
+
+  describe '#call' do
+    it 'does it' do
+      r = client.call(
+        endpoint: 'get',
+        params: { count: 1, tag: 'schedule' }
+      )
+
+      expect(r).to be_truthy
+    end
   end
 end
